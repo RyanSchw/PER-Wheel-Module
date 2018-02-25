85ede6a34c82e46f1f5bfd8e12854e9d31e19bc5
William <32017607+williamxin14@users.noreply.github.com>
2018-02-25 02:20:28 -0500

    staged files

---
+++ Debug/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.d
@@ -0,0 +1,59 @@
+Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.o: \
+ ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c \
+ ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Inc/FreeRTOSConfig.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Inc/main.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/William/Documents/GitHub/PER-Wheel-Module/Drivers/CMSIS/Include/cmsis_gcc.h
+
+../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Inc/FreeRTOSConfig.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Inc/main.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+
+C:/Users/William/Documents/GitHub/PER-Wheel-Module/Drivers/CMSIS/Include/cmsis_gcc.h:
